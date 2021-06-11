98fcf79c48be41f562ef4d7c38051f928daf76dd
Niels <info@heimseiten.de>
2021-06-11 11:46:01 +0200

    Language added

--- src/Resources/contao/languages/en/default.php
+++ src/Resources/contao/languages/en/default.php
@@ -1,3 +1,3 @@
 <?php
 
-$GLOBALS['TL_LANG']['tl_member']['user_id'] = 'User-ID';
+$GLOBALS['TL_LANG']['tl_member']['user_id'] = ['User-ID','Will be created automatically with the ID of the backend user with the prefix userid_ when creating a new user. So for example userid_1'];
