071cd5804274e57bf91846e793404d83f439d891
Niels <info@heimseiten.de>
2021-06-11 11:47:49 +0200

    Update default.php

--- src/Resources/contao/languages/de/default.php
+++ src/Resources/contao/languages/de/default.php
@@ -1,3 +1,3 @@
 <?php
 
-$GLOBALS['TL_LANG']['tl_member']['user_id'] = 'User-ID';
+$GLOBALS['TL_LANG']['tl_member']['user_id'] = ['User-ID','Wird beim Neuanlegen automatisch mit der ID des Backend-Benutzers mit dem Prefix userid_ erstellt. Also bspw. userid_1'];
