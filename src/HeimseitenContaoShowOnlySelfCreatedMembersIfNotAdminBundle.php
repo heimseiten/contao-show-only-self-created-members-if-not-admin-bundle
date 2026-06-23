eaa4c4e6c24d255707a2408347671d5c85379921
Niels <info@heimseiten.de>
2026-06-23 10:32:16 +0200

    Modernize bundle layout (src/Resources → project root, AbstractBundle)

--- src/HeimseitenContaoShowOnlySelfCreatedMembersIfNotAdminBundle.php
+++ src/HeimseitenContaoShowOnlySelfCreatedMembersIfNotAdminBundle.php
@@ -2,32 +2,10 @@
 
 declare(strict_types=1);
 
-/*
- * This file is part of show-only-self-created-members-if-not-admin.
- * 
- * (c) heimseiten.de - Webdesign aus Köln 2021 <info@heimseiten.de>
- * @license GPL-3.0-or-later
- * For the full copyright and license information,
- * please view the LICENSE file that was distributed with this source code.
- * @link https://github.com/heimseiten/contao-show-only-self-created-members-if-not-admin-bundle
- */
-
 namespace Heimseiten\ContaoShowOnlySelfCreatedMembersIfNotAdminBundle;
 
-use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
-/**
- * Class HeimseitenContaoShowOnlySelfCreatedMembersIfNotAdminBundle
- */
-class HeimseitenContaoShowOnlySelfCreatedMembersIfNotAdminBundle extends Bundle
+class HeimseitenContaoShowOnlySelfCreatedMembersIfNotAdminBundle extends AbstractBundle
 {
-	/**
-	 * {@inheritdoc}
-	 */
-	public function build(ContainerBuilder $container): void
-	{
-		parent::build($container);
-		
-	}
 }
